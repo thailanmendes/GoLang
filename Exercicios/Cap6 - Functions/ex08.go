ebac6fccc7f68d336b854ed5daac0649eec45e28
Thailan <thailanmendes88@gmail.com>
2022-08-30 15:52:14 -0300

    Create ex08.go

---
+++ Exercicios/Cap6 - Functions/ex08.go
@@ -0,0 +1,15 @@
+//- Crie uma função que retorna uma função.
+//- Atribua a função retornada a uma variável.
+//- Chame a função retornada.
+
+package main
+
+import "fmt"
+
+func main () {
+	x := func(){
+		fmt.Println("klp[")
+	}
+	x()
+}
+
